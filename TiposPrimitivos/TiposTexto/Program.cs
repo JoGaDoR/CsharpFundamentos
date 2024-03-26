54a59999cca1c454f41eacd3391a8031db167ba2
jogador <thiagojogador171@gmail.com>
2024-03-26 16:26:50 -0300

    feat: tiposTexto

---
+++ TiposPrimitivos/TiposTexto/Program.cs
@@ -0,0 +1,13 @@
+namespace TiposPrimitivos.TiposTexto;
+
+class Program
+{
+    static void Main()
+    {
+        //char - https://learn.microsoft.com/pt-br/dotnet/csharp/language-reference/builtin-types/char
+        char letra = 'T';
+
+        //string = texto
+        string nome = "Thiago";
+    }
+}
