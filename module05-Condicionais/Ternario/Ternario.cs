3bf486b80f1e299e49d7dd7cf4f140c6acf79eae
jogador <thiagojogador171@gmail.com>
2024-03-28 22:27:12 -0300

    feat: comentario

--- module05-Condicionais/Ternario/Ternario.cs
+++ module05-Condicionais/Ternario/Ternario.cs
@@ -8,6 +8,7 @@ namespace CsharpFundamentos.Module05Condicionais.Ternario
             int soma = numero + numero2;
             int subtrair = numero - numero2;
 
+            //exemplo de condicional ternario
             string resultado = soma > 10 ? "Maior" : "Menor";
             Console.WriteLine(resultado);
 
