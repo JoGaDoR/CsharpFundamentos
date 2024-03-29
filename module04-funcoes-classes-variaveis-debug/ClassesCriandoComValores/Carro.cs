130726c921e25e38c6a7fcc75d2f38c7b6345e0a
jogador <thiagojogador171@gmail.com>
2024-03-28 21:11:46 -0300

    feat: propriedade do objeto obrigatoria

--- module04-funcoes-classes-variaveis-debug/ClassesCriandoComValores/Carro.cs
+++ module04-funcoes-classes-variaveis-debug/ClassesCriandoComValores/Carro.cs
@@ -9,3 +9,29 @@ public class Carro{
     public void NomeDoModelo() => Console.WriteLine(Modelo);
     
 }
+
+public class CarroInformacaoObrigatoria01{
+
+    // com o required a propriedade é obrigatória e deve ser informada para evitar erros
+    public required string? Modelo {get; set;}
+    public required DateOnly LancadoEm {get; set;}
+    public required Cor Cor {get; set;}
+
+    public void CorDoCarro() => Console.WriteLine(Cor);
+
+}
+
+public class CarroInformacaoObrigatoria02{
+    
+    public string? Modelo {get; set;}
+    public DateOnly LancadoEm {get; set;}
+
+    public Cor Cor {get; set;}
+
+    public CarroInformacaoObrigatoria02(string modelo, DateOnly lancadoEm, Cor cor){
+        Modelo = modelo;
+        LancadoEm = lancadoEm;
+        Cor = cor;
+    }
+    public void DataDeLancamento () => Console.WriteLine(LancadoEm);
+}
--- module04-funcoes-classes-variaveis-debug/ClassesCriandoComValores/CarroInformacoes.cs
+++ module04-funcoes-classes-variaveis-debug/ClassesCriandoComValores/CarroInformacoes.cs
@@ -1,8 +1,10 @@
 namespace CsharpFundamentos.Module04FuncoesClassesVariaveisDebug.ClassesCriandoComValores;
 
-public class CarroInformacoes{
+public class CarroInformacoes
+{
 
-    public static void Acessar(){
+    public static void Acessar()
+    {
 
         // var carro = new Carro();
 
@@ -10,12 +12,36 @@ public class CarroInformacoes{
         // carro.LancadoEm = new DateOnly(2023, 01, 21);
         // carro.Cor = Cor.VERMELHO;
 
-        var carro = new Carro(){
+        var carro = new Carro()
+        {
             Modelo = "Lamborghini",
             LancadoEm = new DateOnly(2023, 01, 21),
-            Cor = Cor.VERMELHO
+            Cor = Cor.AMARELO
         };
 
         carro.NomeDoModelo();
+
+
+        //tornar a propriedade obrigatoria
+        //EXEMPLO 01
+
+        var carroExemplo01 = new CarroInformacaoObrigatoria01
+        {
+            Cor = Cor.VERMELHO,
+            LancadoEm = new DateOnly(2021, 10, 21),
+            Modelo = "Ferrari"
+        };
+
+        carroExemplo01.CorDoCarro();
+
+
+        //tornar a propriedade obrigatoria
+        //EXEMPLO 02
+
+        var carroExemplo02 = new CarroInformacaoObrigatoria02("Aston Martin", new DateOnly(2023, 03, 18), Cor.AZUL);
+
+        carroExemplo02.DataDeLancamento();
+
+
     }
 }
